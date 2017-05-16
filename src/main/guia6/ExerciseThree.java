e1a2dd2f08e717db7faa00346a488048dd7946b9
Tomas Piaggio <tomas.piaggio12@gmail.com>
2017-05-15 22:33:16 -0300

    Started exercise 3 implementation

---
+++ src/main/guia6/ExerciseThree.java
@@ -0,0 +1,77 @@
+package main.guia6;
+
+import main.guia6.interfaces.Calculator;
+import main.guia6.interfaces.Exercise3;
+
+/**
+ * Created by Tomas on 5/15/17.
+ */
+public class ExerciseThree implements Exercise3{
+    @Override
+    public double[][] exerciseAI(double[][] matrixA, double[] vectorX, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseAII(double[][] matrixA, double[][] matrixB, Calculator calculator) {
+        for (int i = 0; i < matrixA.length; i++)
+            for (int j = 0; j < matrixA[i].length; j++)
+                matrixB[i][j] += matrixA[i][j];
+        return matrixB;
+    }
+
+    @Override
+    public double[][] exerciseAIII(double[][] matrixA, double[][] matrixB, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseBI(double[][] matrixA, double[] vectorX, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseBII(double[][] matrixA, double[][] matrixB, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseBIII(double[][] matrixA, double[][] matrixB, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseCI(double[][] matrixA, double[] vectorX, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseCII(double[][] matrixA, double[][] matrixB, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseCIII(double[][] matrixA, double[][] matrixB, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseDI(double[][] matrixA, int k1A, int k2A, double[] vectorX, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseDII(double[][] matrixA, int k1A, int k2A, double[][] matrixB, int k1B, int k2B, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseDIII(double[][] matrixA, int k1A, int k2A, double[][] matrixB, int k1B, int k2B, Calculator calculator) {
+        return new double[0][];
+    }
+
+    @Override
+    public double[][] exerciseE(double[][] matrixA, double[][] matrixB, Calculator calculator) {
+        return new double[0][];
+    }
+}
