5b9be291b79ecedbbcd43fe97ac444982549b1b6
Tomas Piaggio <tomas.piaggio12@gmail.com>
2017-05-23 10:56:10 -0300

    TP4 interfaces added to the project

---
+++ src/main/guia7/interfaces/Calculator.java
@@ -0,0 +1,15 @@
+public interface Calculator{
+	
+	// The equivalent for sum(5,10) is 5+10=15;
+	double sum(double a, double b);
+
+	// The equivalent for subtraction(5,10) is 5-10=-5;
+	double subtraction(double a, double b);
+
+	// The equivalent for multiplication(5,10) is 5*10=-50;
+	double multiplication(double a, double b);
+
+	// The equivalent for division(5,10) is 5/10=0.5;
+	double division(double a, double b);	
+
+}
---
+++ src/main/guia7/interfaces/TP4.java
@@ -0,0 +1,19 @@
+public interface TP4{
+
+	double[] exercise1(double[][] coefficients, double[] independentTerms);
+
+	double[] exercise2(double[][] coefficients, double[] independentTerms);
+
+	double[] exercise5WithoutPivoteo(double[][] coefficients, double[] independentTerms);
+
+	double[] exercise5PartialPivoteo(double[][] coefficients, double[] independentTerms);
+
+	double[] exercise6(double[][] coefficients, double[] independentTerms, Calculator calculator);
+
+	double[] exercise7(double[][] coefficients, double[] independentTerms, Calculator calculator);
+
+	double[][] exercise8(double[][] coefficients);
+
+	double[] exercise9(double[][] coefficients, double[] independentTerms);
+
+}
